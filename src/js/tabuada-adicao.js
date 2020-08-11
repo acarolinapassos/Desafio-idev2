07c3801572cfa4ce1a26cc4e89128ad649c96693
Letpmartins <leticiapmartins08@gmail.com>
2020-08-11 20:06:40 -0300

    first commit

---
+++ src/js/tabuada-adicao.js
@@ -0,0 +1,11 @@
+let totalGeral = 0;
+let fatorMultiplicador = 7;
+let indice = 0;
+
+totalGeral += fatorMultiplicador * indice;
+console.log(`${fatorMultiplicador} + ${indice} : ${fatorMultiplicador + indice}`);
+indice++;
+totalGeral += fatorMultiplicador * indice;
+console.log(`${fatorMultiplicador} + ${indice} : ${fatorMultiplicador + indice}`);
+
+//Complete até o 10

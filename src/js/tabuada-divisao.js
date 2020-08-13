3835714dfb12e1e6ed757bc964f769c29c886327
Letícia P. Martins <leticiapmartins08@gmail.com>
2020-08-13 01:20:11 +0000

    minor fixes

--- src/js/tabuada-divisao.js
+++ src/js/tabuada-divisao.js
@@ -3,38 +3,36 @@ let fatorMultiplicador = 7;
 let indice = 1;
 
 totalGeral += fatorMultiplicador / indice;
-console.log(`${fatorMultiplicador} / ${indice} : ${fatorMultiplicador / indice}`); //1
+console.log(`${fatorMultiplicador} / ${indice} = ${fatorMultiplicador / indice}`); //1
 indice++;
 totalGeral += fatorMultiplicador / indice;
-console.log(`${fatorMultiplicador} / ${indice} : ${fatorMultiplicador / indice}`); //2
+console.log(`${fatorMultiplicador} / ${indice} = ${fatorMultiplicador / indice}`); //2
 indice++;
 totalGeral += fatorMultiplicador / indice;
-console.log(`${fatorMultiplicador} / ${indice} : ${fatorMultiplicador / indice}`); //3
+console.log(`${fatorMultiplicador} / ${indice} = ${fatorMultiplicador / indice}`); //3
 indice++;
 totalGeral += fatorMultiplicador / indice;
-console.log(`${fatorMultiplicador} / ${indice} : ${fatorMultiplicador / indice}`); //4
+console.log(`${fatorMultiplicador} / ${indice} = ${fatorMultiplicador / indice}`); //4
 indice++;
 
 totalGeral += fatorMultiplicador / indice;
-console.log(`${fatorMultiplicador} / ${indice} : ${fatorMultiplicador / indice}`); //5
+console.log(`${fatorMultiplicador} / ${indice} = ${fatorMultiplicador / indice}`); //5
 indice++;
 
 totalGeral += fatorMultiplicador / indice;
-console.log(`${fatorMultiplicador} / ${indice} : ${fatorMultiplicador / indice}`); //6
+console.log(`${fatorMultiplicador} / ${indice} = ${fatorMultiplicador / indice}`); //6
 indice++;
 totalGeral += fatorMultiplicador / indice;
-console.log(`${fatorMultiplicador} / ${indice} : ${fatorMultiplicador / indice}`); //7
+console.log(`${fatorMultiplicador} / ${indice} = ${fatorMultiplicador / indice}`); //7
 indice++;
 totalGeral += fatorMultiplicador / indice;
-console.log(`${fatorMultiplicador} / ${indice} : ${fatorMultiplicador / indice}`); //8
+console.log(`${fatorMultiplicador} / ${indice} = ${fatorMultiplicador / indice}`); //8
 indice++;
 totalGeral += fatorMultiplicador / indice;
-console.log(`${fatorMultiplicador} / ${indice} : ${fatorMultiplicador / indice}`); //9
+console.log(`${fatorMultiplicador} / ${indice} = ${fatorMultiplicador / indice}`); //9
 indice++;
 totalGeral += fatorMultiplicador / indice;
-console.log(`${fatorMultiplicador} / ${indice} : ${fatorMultiplicador / indice}`); //10
-indice++;
+console.log(`${fatorMultiplicador} / ${indice} = ${fatorMultiplicador / indice}`); //10
 
 
-
-console.log(totalGeral);
+console.log(`Soma total: ${totalGeral}`);
--- src/js/tabuada-subtracao.js
+++ src/js/tabuada-subtracao.js
@@ -3,38 +3,38 @@ let fatorMultiplicador = 7;
 let indice = 0;
 
 totalGeral += fatorMultiplicador - indice;
-console.log(`${fatorMultiplicador} - ${indice} : ${fatorMultiplicador - indice}`); //0
+console.log(`${fatorMultiplicador} - ${indice} = ${fatorMultiplicador - indice}`); //0
 indice++;
 totalGeral += fatorMultiplicador - indice;
-console.log(`${fatorMultiplicador} - ${indice} : ${fatorMultiplicador - indice}`); //1
+console.log(`${fatorMultiplicador} - ${indice} = ${fatorMultiplicador - indice}`); //1
 indice++;
 totalGeral += fatorMultiplicador - indice;
-console.log(`${fatorMultiplicador} - ${indice} : ${fatorMultiplicador - indice}`); //2
+console.log(`${fatorMultiplicador} - ${indice} = ${fatorMultiplicador - indice}`); //2
 indice++;
 totalGeral += fatorMultiplicador - indice;
-console.log(`${fatorMultiplicador} - ${indice} : ${fatorMultiplicador - indice}`); //3
+console.log(`${fatorMultiplicador} - ${indice} = ${fatorMultiplicador - indice}`); //3
 indice++;
 totalGeral += fatorMultiplicador - indice;
-console.log(`${fatorMultiplicador} - ${indice} : ${fatorMultiplicador - indice}`); //4
+console.log(`${fatorMultiplicador} - ${indice} = ${fatorMultiplicador - indice}`); //4
 indice++;
 
 totalGeral += fatorMultiplicador - indice;
-console.log(`${fatorMultiplicador} - ${indice} : ${fatorMultiplicador - indice}`); //5
+console.log(`${fatorMultiplicador} - ${indice} = ${fatorMultiplicador - indice}`); //5
 indice++;
 totalGeral += fatorMultiplicador - indice;
-console.log(`${fatorMultiplicador} - ${indice} : ${fatorMultiplicador - indice}`); //6
+console.log(`${fatorMultiplicador} - ${indice} = ${fatorMultiplicador - indice}`); //6
 indice++;
 totalGeral += fatorMultiplicador - indice;
-console.log(`${fatorMultiplicador} - ${indice} : ${fatorMultiplicador - indice}`); //7
+console.log(`${fatorMultiplicador} - ${indice} = ${fatorMultiplicador - indice}`); //7
 indice++;
 totalGeral += fatorMultiplicador - indice;
-console.log(`${fatorMultiplicador} - ${indice} : ${fatorMultiplicador - indice}`); //8
+console.log(`${fatorMultiplicador} - ${indice} = ${fatorMultiplicador - indice}`); //8
 indice++;
 totalGeral += fatorMultiplicador - indice;
-console.log(`${fatorMultiplicador} - ${indice} : ${fatorMultiplicador - indice}`); //9
+console.log(`${fatorMultiplicador} - ${indice} = ${fatorMultiplicador - indice}`); //9
 indice++;
 totalGeral += fatorMultiplicador - indice;
-console.log(`${fatorMultiplicador} - ${indice} : ${fatorMultiplicador - indice}`); //10
+console.log(`${fatorMultiplicador} - ${indice} = ${fatorMultiplicador - indice}`); //10
 indice++;
 
-console.log(totalGeral);
+console.log(`Soma total: ${totalGeral}`);
